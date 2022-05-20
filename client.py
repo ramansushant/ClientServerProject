252eb97c2932dcf95ea98e9ee61cb2da43d36279
sushantraman <sushant@appsflyer.com>
2022-05-21 00:17:05 +0100

    Exception Handling Added

--- client.py
+++ client.py
@@ -5,12 +5,16 @@ from dict2xml import dict2xml
 from cryptography.fernet import Fernet
 import os
 
+# Metadata seperator is used so that Filename and Content Type can be split on Server
 METADATA_SEPARATOR = "$$"
 
 class Client:
+    # This class connects the Client to Server and send the files both in Encrypted and Decrypted Form
 
     def __init__(self):
+        # Socket to connect to Server
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Global Variables that are accessed across functions
         self.data = dict()
         self.filename = ""
         self.key_filename = 'key.key'
@@ -19,20 +23,24 @@ class Client:
         self.encrypted_text = ""
 
     def connect_to_server(self):
-
         host = '0.0.0.0'  # Standard loopback interface address (localhost)
         port = 8000  # Port to listen on (non-privileged ports are > 1023)
-        self.s.connect((host, port))
+        try:
+            self.s.connect((host, port)) # To connect client to server on specified Host and Port
+        except:
+            print("Connection failed")
 
     def disconnect_server(self):
-        self.s.close()
+        self.s.close() # To disconnect client from server
 
     def dict(self):
+        # User Input in the Dictionary Data Type
         key = input("Enter Key: ")
         value = input("Enter Value: ")
         self.data[key] = [value]
         print(self.data)
 
+        # Input to Serialize the Dictionary in the user chosen format
 
         serialize_format = input("Enter the Format: ")
 
@@ -54,66 +62,83 @@ class Client:
                 self.filename = 'output.xml'
                 print("Serialization in XML format done")
 
+        else:
+            print("Incorrect Format. Try Again")
+
 
     def text_file(self):
-        self.filename = input("Enter Filename to be sent: ")
-        self.encrypted = input("Enter E to send Encrypted Version else P: ")
-        if self.encrypted == "E":
-            if os.path.exists(self.key_filename):
-                with open(self.key_filename, "rb") as key_file:
-                    key = key_file.read()
-            else:
-                key = Fernet.generate_key()
-                with open(self.key_filename, "wb") as key_file:
-                    key_file.write(key)
+        try:
+            # User Input on the File that client wants to send to the server
+            self.filename = input("Enter Filename to be sent: ")
+            self.encrypted = input("Enter E to send Encrypted Version else P: ")
+            # User Input on the File that client wants to send to the server
+            if self.encrypted == "E":
+                if os.path.exists(self.key_filename):
+                    with open(self.key_filename, "rb") as key_file:
+                        key = key_file.read()
+                else:
+                    key = Fernet.generate_key()
+                    with open(self.key_filename, "wb") as key_file:
+                        key_file.write(key)
 
-            f = Fernet(key)
-            with open(self.filename, 'rb') as file:
-                text_in_file = file.read()
+                f = Fernet(key)
+                with open(self.filename, 'rb') as file:
+                    text_in_file = file.read()
 
-            self.encrypted_text = f.encrypt(text_in_file)
-            self.encrypted_filename = 'encrypted_' + self.filename
+                self.encrypted_text = f.encrypt(text_in_file)
+                self.encrypted_filename = 'encrypted_' + self.filename
 
-            with open(self.encrypted_filename, 'wb') as encrypted_file:
-                encrypted_file.write(self.encrypted_text)
+                with open(self.encrypted_filename, 'wb') as encrypted_file:
+                    encrypted_file.write(self.encrypted_text)
 
-            print("Encrypted file saved as: ", self.encrypted_filename)
+                print("Encrypted file saved as: ", self.encrypted_filename)
+
+        except Exception as ex:
+            print("Error Reading the File " + str(ex))
+
 
     def send_data_to_server(self):
-        if self.encrypted == "E":
-            e = "Encrypted"
-            self.s.send(f"{e}{METADATA_SEPARATOR}{self.encrypted_filename}".encode())
-            with open(self.encrypted_filename, "rb") as file:
+        try:
+            if self.encrypted == "E":
+                e = "Encrypted"
+                self.s.send(f"{e}{METADATA_SEPARATOR}{self.encrypted_filename}".encode())
+                with open(self.encrypted_filename, "rb") as file:
                     data = file.read(4096)
                     while data:
                         self.s.send(data)
                         data = file.read(4096)
-            print("Encrypted Data Sent Successfully")
+                print("Encrypted Data Sent Successfully")
 
 
-        else:
-            self.s.send(f"Non-Encrypted{METADATA_SEPARATOR}{self.filename}".encode())
-            with open(self.filename, "rb") as file:
-                data = file.read(4096)
-                while data:
-                    self.s.send(data)
+            else:
+                self.s.send(f"Non-Encrypted{METADATA_SEPARATOR}{self.filename}".encode())
+                with open(self.filename, "rb") as file:
                     data = file.read(4096)
-            print("Non-Encrypted Data Sent Successfully")
+                    while data:
+                        self.s.send(data)
+                        data = file.read(4096)
+                print("Non-Encrypted Data Sent Successfully")
 
-
-
+        except Exception as ex:
+            print("Error Sending the File " + str(ex))
 
 if __name__ == "__main__":
+
     client = Client()
     client.connect_to_server()
+
     user_input = input("Enter D to create Dictionary or Enter T to send the Text File: ")
+
     if user_input == 'D':
         client.dict()
         client.send_data_to_server()
 
-    if user_input == 'T':
+    elif user_input == 'T':
         client.text_file()
         client.send_data_to_server()
+
+    else:
+        print("Incorrect Input")
 
     client.disconnect_server()
 
--- server.py
+++ server.py
@@ -23,8 +23,14 @@ class Server:
         host = '0.0.0.0'
         port = 8000
         print("CONFIGURE: PRINT CONTENT")
+
         self.configuration = input("Enter S for SCREEN or Enter F for FILE: ")
-        print(f"Server Configured in Mode: {self.configuration}")
+        if self.configuration == "S" or self.configuration == "F":
+            print(f"Server Configured in Mode: {self.configuration}")
+        else:
+            print("Invalid Input Try Again")
+            self.configuration = input("Enter S for SCREEN or Enter F for FILE: ")
+
         self.s.bind((host, port))
         self.s.listen()
         c, addr = self.s.accept()
@@ -32,39 +38,46 @@ class Server:
         threading.Thread(target=self.receive_data, args=(c, addr)).start()
 
     def receive_data(self, c, addr):
-        metadata = c.recv(4096).decode()
-        encryption, filename = metadata.split(METADATA_SEPARATOR)
-        data = c.recv(4096)
+        try:
+            metadata = c.recv(4096).decode()
+            encryption, filename = metadata.split(METADATA_SEPARATOR)
+            data = c.recv(4096)
 
-        if self.configuration == 'S':
+            if self.configuration == 'S':
+                try:
+                    if encryption == 'Non-Encrypted':
+                        print('MESSAGE: ' + data.decode())
 
-            if encryption == 'Non-Encrypted':
-                print('MESSAGE: ' + data.decode())
+                    else:
+                        if os.path.exists(self.key_filename):
+                            with open(self.key_filename, "rb") as key_file:
+                                key = key_file.read()
+                        f = Fernet(key)
+                        self.decrypted_text = f.decrypt(data)
+                        print(self.decrypted_text)
 
-            else:
-                if os.path.exists(self.key_filename):
-                    with open(self.key_filename, "rb") as key_file:
-                        key = key_file.read()
-                f = Fernet(key)
-                self.decrypted_text = f.decrypt(data)
-                print(self.decrypted_text)
+                except UnicodeDecodeError:
+                    print('The file is in binary formart and cannot display on screen.')
 
-        elif self.configuration == 'F':
-            if encryption == 'Non-Encrypted':
-                self.server_filename = 's_' + filename
-                with open(self.server_filename, "wb") as received_file:
-                    received_file.write(data)
-            else:
-                if os.path.exists(self.key_filename):
-                    with open(self.key_filename, "rb") as key_file:
-                        key = key_file.read()
-                f = Fernet(key)
-                self.decrypted_text = f.decrypt(data)
-                self.server_filename = 's_' + filename
-                print(self.server_filename)
-                with open(self.server_filename, "wb") as received_file:
-                    received_file.write(self.decrypted_text)
-                    print("File Saved as:" + self.server_filename)
+            elif self.configuration == 'F':
+                if encryption == 'Non-Encrypted':
+                    self.server_filename = 's_' + filename
+                    with open(self.server_filename, "wb") as received_file:
+                        received_file.write(data)
+                else:
+                    if os.path.exists(self.key_filename):
+                        with open(self.key_filename, "rb") as key_file:
+                            key = key_file.read()
+                    f = Fernet(key)
+                    self.decrypted_text = f.decrypt(data)
+                    self.server_filename = 's_' + filename
+                    print(self.server_filename)
+                    with open(self.server_filename, "wb") as received_file:
+                        received_file.write(self.decrypted_text)
+                        print("File Saved as:" + self.server_filename)
+
+        except Exception as exception:
+            print("Could not read the data from the Socket. " + str(exception))
 
         c.close()
 
